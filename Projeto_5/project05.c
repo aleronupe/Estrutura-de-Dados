f1da13697277773bbc29ba9a79e9e5934a4fe830
Daniel Maike <danmke@hotmail.com>
2018-06-14 19:41:55 -0300

    inserindo função para imprimir árvore e para verificar se a árvore é cheia

--- Projeto_5/project05.c
+++ Projeto_5/project05.c
@@ -1,17 +1,261 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct binary_tree {
+struct node {
   int info;
-  struct binary_tree *left;
-  struct binary_tree *right;
-} BinaryTree;
+  struct node *left;
+  struct node *right;
+};
+
+typedef struct node *BinaryTree;
+
+#define MAX_HEIGHT 1000
+int lprofile[MAX_HEIGHT];
+int rprofile[MAX_HEIGHT];
+#define INFINITY (1<<20)
+
+typedef int ELEMENT;
+
+typedef struct BSTNode_struct BSTNode;
+
+struct BSTNode_struct {
+    BSTNode *ptesq, *ptdir;
+
+    //length of the edge from this no to its children
+    int edge_length;
+
+    int altura;
+
+    ELEMENT element;
+
+    //-1=I am left, 0=I am root, 1=right
+    int parent_dir;
+
+    //max supported unit32 in dec, 10 digits max
+    char label[11];
+};
+
+//adjust gap between left and right nos
+int gap = 3;
+
+//used for printing next no in the same level,
+//this is the x coordinate of the next char printed
+int print_next;
+
+int MIN(int X, int Y) {
+    return ((X) < (Y)) ? (X) : (Y);
+}
+
+int MAX(int X, int Y) {
+    return ((X) > (Y)) ? (X) : (Y);
+}
+
+BSTNode *build_ascii_tree_recursive(struct node *arvore) {
+    BSTNode *no;
+
+    if (arvore == NULL) return NULL;
+
+    no = malloc(sizeof(BSTNode));
+    no->ptesq = build_ascii_tree_recursive(arvore->left);
+    no->ptdir = build_ascii_tree_recursive(arvore->right);
+
+    if (no->ptesq != NULL) {
+        no->ptesq->parent_dir = -1;
+    }
+
+    if (no->ptdir != NULL) {
+        no->ptdir->parent_dir = 1;
+    }
+
+    sprintf(no->label, "%d", arvore->info);
+    no->element = strlen(no->label);
+
+    return no;
+}
+
+
+//Copy the tree into the ascii no structre
+BSTNode *build_ascii_tree(struct node *arvore) {
+    BSTNode *no;
+    if (arvore == NULL) return NULL;
+    no = build_ascii_tree_recursive(arvore);
+    no->parent_dir = 0;
+    return no;
+}
+
+//Free all the nos of the given tree
+void free_ascii_tree(BSTNode *no) {
+    if (no == NULL) return;
+    free_ascii_tree(no->ptesq);
+    free_ascii_tree(no->ptdir);
+    free(no);
+}
+
+//The following function fills in the lprofile array for the given tree.
+//It assumes that the center of the label of the root of this tree
+//is located at a position (x,y).  It assumes that the edge_length
+//fields have been computed for this tree.
+void compute_lprofile(BSTNode *no, int x, int y) {
+    int i, eh_esq;
+    if (no == NULL) return;
+    eh_esq = (no->parent_dir == -1);
+    lprofile[y] = MIN(lprofile[y], x - ((no->element - eh_esq) / 2));
+    if (no->ptesq != NULL) {
+        for (i = 1; i <= no->edge_length && y + i < MAX_HEIGHT; i++) {
+            lprofile[y + i] = MIN(lprofile[y + i], x - i);
+        }
+    }
+    compute_lprofile(no->ptesq, x - no->edge_length - 1, y + no->edge_length + 1);
+    compute_lprofile(no->ptdir, x + no->edge_length + 1, y + no->edge_length + 1);
+}
+
+void compute_rprofile(BSTNode *no, int x, int y) {
+    int i, n_eh_esq;
+    if (no == NULL) return;
+    n_eh_esq = (no->parent_dir != -1);
+    rprofile[y] = MAX(rprofile[y], x + ((no->element - n_eh_esq) / 2));
+    if (no->ptdir != NULL) {
+        for (i = 1; i <= no->edge_length && y + i < MAX_HEIGHT; i++) {
+            rprofile[y + i] = MAX(rprofile[y + i], x + i);
+        }
+    }
+    compute_rprofile(no->ptesq, x - no->edge_length - 1, y + no->edge_length + 1);
+    compute_rprofile(no->ptdir, x + no->edge_length + 1, y + no->edge_length + 1);
+}
+
+//This function fills in the edge_length and
+//altura fields of the specified tree
+void filledge(BSTNode *no) {
+    int h, hmin, i, delta;
+    if (no == NULL) return;
+    filledge(no->ptesq);
+    filledge(no->ptdir);
+
+   /* first fill in the edge_length of no */
+   if (no->ptdir == NULL && no->ptesq == NULL) {
+        no->edge_length = 0;
+    }
+    else {
+        if (no->ptesq != NULL) {
+            for (i = 0; i < no->ptesq->altura && i < MAX_HEIGHT; i++) {
+                rprofile[i] = -INFINITY;
+            }
+            compute_rprofile(no->ptesq, 0, 0);
+            hmin = no->ptesq->altura;
+        }
+        else {
+            hmin = 0;
+        }
+        if (no->ptdir != NULL) {
+            for (i = 0; i < no->ptdir->altura && i < MAX_HEIGHT; i++) {
+                lprofile[i] = INFINITY;
+            }
+            compute_lprofile(no->ptdir, 0, 0);
+            hmin = MIN(no->ptdir->altura, hmin);
+        }
+        else {
+            hmin = 0;
+        }
+        delta = 4;
+        for (i = 0; i < hmin; i++) {
+            delta = MAX(delta, gap + 1 + rprofile[i] - lprofile[i]);
+        }
+
+        //If the no has two children of altura 1, then we allow the
+        //two leaves to be within 1, instead of 2
+        if (((no->ptesq != NULL && no->ptesq->altura == 1) ||
+             (no->ptdir != NULL && no->ptdir->altura == 1)) && delta > 4) {
+            delta--;
+        }
+
+        no->edge_length = ((delta + 1) / 2) - 1;
+    }
+
+    //now fill in the altura of no
+    h = 1;
+    if (no->ptesq != NULL) {
+        h = MAX(no->ptesq->altura + no->edge_length + 1, h);
+    }
+    if (no->ptdir != NULL) {
+        h = MAX(no->ptdir->altura + no->edge_length + 1, h);
+    }
+    no->altura = h;
+}
+
+//This function prints the given level of the given tree, assuming
+//that the no has the given x cordinate.
+void printLevel(BSTNode *no, int x, int level) {
+    int i, isleft;
+    if (no == NULL) return;
+    isleft = (no->parent_dir == -1);
+    if (level == 0) {
+        for (i = 0; i < (x - print_next - ((no->element - isleft) / 2)); i++) {
+            printf(" ");
+        }
+        print_next += i;
+        printf("%s", no->label);
+        print_next += no->element;
+    }
+    else if (no->edge_length >= level) {
+        if (no->ptesq != NULL) {
+            for (i = 0; i < (x - print_next - (level)); i++) {
+                printf(" ");
+            }
+            print_next += i;
+            printf("/");
+            print_next++;
+        }
+        if (no->ptdir != NULL) {
+            for (i = 0; i < (x - print_next + (level)); i++) {
+                printf(" ");
+            }
+            print_next += i;
+            printf("\\");
+            print_next++;
+        }
+    }
+    else {
+        printLevel(no->ptesq,
+                   x - no->edge_length - 1,
+                   level - no->edge_length - 1);
+        printLevel(no->ptdir,
+                   x + no->edge_length + 1,
+                   level - no->edge_length - 1);
+    }
+}
+
+//prints ascii tree for given Tree structure
+void showTree(BinaryTree *raiz) {
+    BSTNode *proot;
+    int xmin, i;
+    if (raiz == NULL) return;
+    proot = build_ascii_tree(*raiz);
+    filledge(proot);
+    for (i = 0; i < proot->altura && i < MAX_HEIGHT; i++) {
+        lprofile[i] = INFINITY;
+    }
+    compute_lprofile(proot, 0, 0);
+    xmin = 0;
+    for (i = 0; i < proot->altura && i < MAX_HEIGHT; i++) {
+        xmin = MIN(xmin, lprofile[i]);
+    }
+    for (i = 0; i < proot->altura; i++) {
+        print_next = 0;
+        printLevel(proot, -xmin, i);
+        printf("\n");
+    }
+    if (proot->altura >= MAX_HEIGHT) {
+        printf("(This tree is taller than %d, and may be drawn incorrectly.)\n", MAX_HEIGHT);
+    }
+    free_ascii_tree(proot);
+}
 
 BinaryTree *createBinaryTree();
 
 void insertBinaryTree(BinaryTree *root, int value);
 
-void freeNode(BinaryTree *node);
+void freeNode(struct node *no);
 
 void freeBinaryTree(BinaryTree *root);
 
@@ -24,9 +268,11 @@ void printInOrder(BinaryTree *root);
 void printPostOrder(BinaryTree *root);
 // postOrder: visita o filho da esquerda, o filho da direita e a raiz
 
-BinaryTree *removeCurrent(BinaryTree *current);
+struct node *removeCurrent(struct node *current);
 
 void removeValue(BinaryTree *root, int value);
+
+int isFull(struct node *root);
 
 void searchValue(BinaryTree *root, int value);
 
@@ -35,9 +281,52 @@ BinaryTree *loadTreeFromFile();
 ////////////////////////// main /////////////////////////////
 int main () {
 
+  int full;
+
   BinaryTree *root = createBinaryTree(); // Aponta para o primeiro elemento da arvore.
 
-  // root = loadTreeFromFile();
+  root = loadTreeFromFile();
+
+  /*
+  int selector;
+
+  while(selector != 10) {
+
+    printf("\n-------------------------------------------------------------------------------------------\n");
+    printf("---\t\t\tSELECIONE A OPÇÃO DESEJADA:\t\t\t\t\t---\n");
+    printf("-------------------------------------------------------------------------------------------\n");
+    printf("---\t\t\t(1) SHOW TREE\t\t\t\t\t---\n");
+    printf("---\t\t\t(2) IS FULL\t\t\t\t\t\t---\n");
+    printf("---\t\t\t(3) SEARCH VALUE\t\t\t\t\t\t---\n");
+    printf("---\t\t\t(4) GET HEIGHT\t\t---\n");
+    printf("---\t\t\t(5) REMOVE VALUE\t\t---\n");
+    printf("---\t\t\t(6) PRINT PREORDER\t\t---\n");
+    printf("---\t\t\t(7) PRINT INORDER\t\t---\n");
+    printf("---\t\t\t(8) PRINT POSTORDER\t\t---\n");
+    printf("---\t\t\t(9) BALANCE TREE\t\t---\n");
+    printf("---\t\t\t(10) EXIT\t\t\t\t\t\t\t---\n");
+    printf("-------------------------------------------------------------------------------------------\n");
+
+    scanf("%d\n", &selector);
+
+    if(selector == 1) {
+
+    } else if(selector == 2) {
+      full = isFull(*root);
+
+      if(full == 1) {
+        printf("A árvore é cheia!\n");
+      } else {
+        printf("A árvore não é cheia!\n");
+      }
+    } else {
+      printf("Invalid option\n");
+    }
+  }
+
+  */
+
+
 
   // printPreOrder(root);
 
@@ -66,7 +355,8 @@ void insertBinaryTree(BinaryTree *root, int value) {
   if(root == NULL) {
     return;
   }
-  BinaryTree *new = (BinaryTree*) malloc(sizeof(BinaryTree));
+  struct node *new;
+  new = (struct node*) malloc(sizeof(struct node));
   if(new == NULL) { // confere a alocacao
     return;
   }
@@ -77,8 +367,8 @@ void insertBinaryTree(BinaryTree *root, int value) {
   if(*root == NULL) {
     *root = new;
   } else {
-    BinaryTree *current = *root;
-    BinaryTree *previous = NULL;
+    struct node *current = *root;
+    struct node *previous = NULL;
 
     while(current != NULL) { // quando tiver achado aonde ira ser inserido, para o while
       previous = current; // salva o atual pois o atual se tornara um dos filhos
@@ -102,14 +392,14 @@ void insertBinaryTree(BinaryTree *root, int value) {
   }
 }
 ///////////////////////////////////////////////////////////////
-void freeNode(BinaryTree *node) {
-  if(node == NULL) { // verifica se a alocacao deu certo
+void freeNode(struct node *no) {
+  if(no == NULL) { // verifica se a alocacao deu certo
     return;
   }
-  freeNode(node->left); // vai a esquerda e libera
-  freeNode(node->right); // vai a direita e libera
-  free(node); // libera o no apos liberar tudo a direita e a esquerda
-  node = NULL;
+  freeNode(no->left); // vai a esquerda e libera
+  freeNode(no->right); // vai a direita e libera
+  free(no); // libera o no apos liberar tudo a direita e a esquerda
+  no = NULL;
 }
 ///////////////////////////////////////////////////////////////
 void freeBinaryTree(BinaryTree *root) {
@@ -167,8 +457,8 @@ void printPostOrder(BinaryTree *root) {
   }
 }
 ///////////////////////////////////////////////////////////////
-BinaryTree *removeCurrent(BinaryTree *current) {
-  BinaryTree *node1, *node2;
+struct node *removeCurrent(struct node *current) {
+  struct node *node1, *node2;
   if(current->left == NULL) {
     // sem filho da esquerda, apontar para o filho da direita (trata no folha e com 1 filho)
     node2 = current->right;
@@ -196,11 +486,11 @@ void removeValue(BinaryTree *root, int value) {
   if(root == NULL) {
     return;
   }
-  BinaryTree *previous = NULL;
-  BinaryTree *current = *root;
+  struct node *previous = NULL;
+  struct node *current = *root;
   while(current != NULL) {
     if(value == current->info) { // verifica se achou o valor
-      if(value == *root) { // verifica se eh a raiz
+      if(current == *root) { // verifica se eh a raiz
         *root = removeCurrent(current);
       } else {
         if(previous->right == current) { // verifica se eh o da direita
@@ -219,22 +509,42 @@ void removeValue(BinaryTree *root, int value) {
   }
 }
 ///////////////////////////////////////////////////////////////
+int isFull(struct node *root) {
+  // se a arvore eh vazia
+	if (root == NULL)
+		return 1;
+
+	// If leaf node
+	if (root->left == NULL && root->right == NULL)
+		return 1;
+
+	// If both left and right are not NULL, and left & right subtrees
+	// are full
+	if ((root->left) && (root->right))
+		 if(isFull(root->left) && isFull(root->right)) {
+       return 1;
+     }
+
+	// We reach here when none of the above if conditions work
+	return 0;
+}
+///////////////////////////////////////////////////////////////
 void searchValue(BinaryTree *root, int value) {
   int level = 1;
   if(root == NULL) {
     printf("O valor não está presente na árvore!\n");
   }
-  BinaryTree *current = *root;
-  BinaryTree *previous = NULL;
+  struct node *current = *root;
+  struct node *previous = NULL;
   while(current != NULL) {
     if(value == current->info) {
       printf("Valor encontrado %d\n", value); // se existe na arvore
       printf("Nível do nó: %d\n", level);
       if(previous != NULL) {
         printf("Pai: %d\n", previous->info);
-        if(previous->right != current->info && previous->right != NULL) {
+        if(previous->right->info != current->info && previous->right != NULL) {
           printf("Irmão: %d\n", previous->right->info);
-        } else if (previous->left != current->info && previous->left != NULL){
+        } else if (previous->left->info != current->info && previous->left != NULL){
           printf("Irmão: %d\n", previous->left->info);
         }
       }
@@ -253,24 +563,26 @@ void searchValue(BinaryTree *root, int value) {
 BinaryTree *loadTreeFromFile() {
   FILE *fp;
   int number;
-  char c;
 
   BinaryTree *root = createBinaryTree();
 
   fp = fopen("BSTs/bst1.txt", "r");
 
   if (fp == NULL) {
-    printf("not found\n");
+    printf("not founded\n");
   }
 
-  while(c != EOF) {
+  while(!feof(fp)) {
     fscanf(fp, "%d", &number);
+    printf("%d\n", number);
     insertBinaryTree(root, number);
   }
 
   fclose(fp);
 
-  printf("Endereço da raíz: %d\n", &root);
+  printf("Endereço da raíz: %d\n", &root); //TODO
+
+  showTree(root);
 
   return root;
 }
